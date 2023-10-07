cccc3e219ac7d53e1e4ffae858d0d3714ecb491d
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2023-10-07 16:32:42 +0200

    Implement navbar without auth

--- src/app/administration/layout/nav-bar/nav-bar.component.ts
+++ src/app/administration/layout/nav-bar/nav-bar.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-// import { AuthService } from 'src/app/core/services/services-auth/auth.service';
-import { MenuItem, MenuSubItem } from './menu-items';
-// import { MenuBuilder } from './menu-builder';
+import { MenuItem } from './menu-items';
+import { Router } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { MenuBuilder } from './menu-builder';
 
 @Component({
     selector: 'app-nav-bar',
@@ -9,27 +10,22 @@ import { MenuItem, MenuSubItem } from './menu-items';
     styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+    dashboardItem = new MenuItem('Dashboard', 'dashboard', 'fa-chart-line');
     logoutItem =  new MenuItem('Log out', '', 'fa-sign-out-alt', () => this.logout());
-
+    builder = new MenuBuilder()
     availableItems: MenuItem[] = [];
     visible = false;
 
     constructor(
-        // private authService: AuthService
+        private router: Router,
+        private oauthService: OAuthService
     ) { }
 
     ngOnInit(): void {
         this.visible = history.state.navBarVisible ?? false;
-        this.availableItems = [new MenuItem('Můj účet', `/admin/users/detail/1`, 'fa-user', () => {})];
-        // [
-        //     this.getMenuItem('Dashboard', '/admin/dashboard', 'fa-chart-line', () => this.getUsersSubItems()),
-        //     new MenuItem('Tickets', '/admin/tickets', 'fa-ticket', () => console.log("tickets")),
-        //     new MenuItem('Years', '/admin/years', 'fa-calendar-days', () => console.log("years"))
-        // ].filter(o => o);
-
-        // this.authService.getCurrentUser().subscribe(user => {
-        //     this.availableItems = new MenuBuilder(this.authService).build(user);
-        // });
+        this.availableItems = this.builder.build()
+        this.availableItems.unshift(this.dashboardItem);
+        this.availableItems.push(this.logoutItem);
     }
 
     toggle(): void {
@@ -41,26 +37,7 @@ export class NavBarComponent implements OnInit {
     }
 
     logout(): void {
-        // this.authService.logout();
-    }
-
-
-    private getMenuItem(
-        name: string, defaultLink: string, icon: string, getSubItems: () => MenuSubItem[]
-    ): MenuItem {
-        const menuItem = new MenuItem(name, defaultLink, icon, () => {});
-        menuItem.subItems.push(...getSubItems());
-
-        return menuItem.subItems.length === 0 ? menuItem : menuItem;
-    }
-
-    getUsersSubItems(): MenuSubItem[] {
-        console.log("years start")
-        const result: MenuSubItem[] = [];
-        result.push(new MenuSubItem('Seznam uživatelů', '/admin/users'));
-        result.push(new MenuSubItem('Vytvořit uživatele', '/admin/users/create'));
-        result.push(new MenuSubItem('Zablokované karty', '/admin/users/cards'));
-        console.log('years end')
-        return result;
+        this.oauthService.logOut(true);
+        this.router.navigate(['/home', { login: true }])
     }
 }
