6f8ff0fa991b82f023234c22460f915fba44c28d
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2024-09-14 10:49:05 +0000

    Hotfix auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
     socketio: "https://api-dev.cutetix.rudickamladez.cz/ws"
   },
   keycloak: {
-    issuer: 'https://auth.lukasmatuska.cz/realms/cutetix',
+    issuer: 'https://auth.lukasmatuska.cz/realms/pohles',
     clientId: 'admin-v2',
     clientSecret: 'poW7Tr4edeEKUXf8dGQjpTymye6L4rRW',
     responseType: 'code',
